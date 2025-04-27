9dff4eb847d77a23369c56c3a89e2231ca4723f0
Mehmet Cihangir <mehmet.cihangir@yahoo.com>
2025-04-27 16:33:15 +0300

    feat: Kiosk Mode: Successfully set Device Owner and admin for CoffeeUI

--- smartnfc/smartnfc.mk
+++ smartnfc/smartnfc.mk
@@ -17,12 +17,18 @@ BOARD_SEPOLICY_UNION += file_contexts \
 # copy init.smartnfc.rc to /system/etc/init folder in the target file system
 # device_owner.xml is used by the Device Policy Manager to designate the CoffeeUI APK as the Device Owner application.
 PRODUCT_COPY_FILES += \
-    device/empa/smartnfc/init.smartnfc.rc:/system/etc/init/init.smartnfc.rc \
+    device/empa/smartnfc/init.smartnfc.rc:system/etc/init/init.smartnfc.rc \
     device/empa/smartnfc/configs/device_owner.xml:system/etc/device_owner.xml \
-    device/empa/smartnfc/configs/privapp-permissions-coffeeui.xml:system/etc/permissions/privapp-permissions-coffeeui.xml
+    device/empa/smartnfc/configs/device_policies.xml:system/etc/device_policies.xml \
+    device/empa/smartnfc/configs/privapp-permissions-coffeeui.xml:system/etc/permissions/privapp-permissions-coffeeui.xml \
+    device/empa/smartnfc/configs/coffeeui_system.prop:system/etc/coffeeui_system.prop
 
 # run init_smartnfc.rc in init stage
 INIT += /system/etc/init/init.smartnfc.rc
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    lockscreen.locked_out_disabled=true \
+    lockscreen.disable_pinning_dialogs=true
 ######################################################################
 # Add custom application
 PRODUCT_PACKAGES += \
@@ -52,9 +58,68 @@ PRODUCT_PACKAGES_REMOVE += \
     VpnDialogs \
     WallpaperCropper
 
-#REMOVE_PACKAGES += \
-
+#Default Launchers
 PRODUCT_PACKAGES_REMOVE += \
     Launcher3 \
+    Launcher3QuickStep \
     Quickstep \
-    Launcher3QuickStep 
+    SettingsIntelligence \
+    AmbientSense
+
+PRODUCT_PACKAGES_REMOVE += \
+    VoiceAIRef \
+    uimgbaservice \
+    aptxui \
+    RamdumpCopyUI \
+    PresenceApp \
+    PerformanceMode \
+    AtFwd2 \
+    Protips \
+    RideModeAudio \
+    ConferenceDialer \
+    QCC \
+    QdcmFF \
+    EmergencyInfo \
+    QesdkSysService \
+    workloadclassifier \
+    PowerSaveMode \
+    WfdService \
+    DCTestApp \
+    imssettings \
+    uceShimService \
+    SnapdragonMusic \
+    ConnectionManagerTestApp \
+    MangoLwm2mApp \
+    FM2 \
+    BTTestApp \
+    WigigSettings \
+    SimContact \
+    QtiTelephonySettings \
+    QtiTelephonyService \
+    QtiTelephony \
+    QSensorTest \
+    WigigTetheringRRO \
+    QColor \
+    colorservice \
+    UnifiedSensorTestApp \
+    TrustZoneAccessService \
+    ConnectionSecurityService \
+    datastatusnotification \
+    DeviceStatisticsService \
+    DeviceInfo \
+    my.tests.snapdragonsdktest \
+    SnapdragonSVA \
+    ModemTestMode \
+    QualcommVoiceActivation \
+    aptxals \
+    aptxacu \
+    atfwd \
+    DynamicDDSService \
+    BTTestApp \
+    xrwifiservice \
+    xrcbservice \
+    xrvdservice \
+    dpmserviceapp \
+    dcf \
+    ODLT 
+
